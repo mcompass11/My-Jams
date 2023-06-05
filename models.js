856714c57b04017bd807e713be0abc7f6c79741e
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2023-06-05 00:35:11 -0400

    added in mongoose

---
+++ models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+let albumSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Artist: {
+    Name: String,
+    City: String,
+    Birthyear: Date,
+    Albums: [String],
+    Image: String 
+  },
+  Genre: String,
+  Year: Date,
+  Trackslist: [String],
+  Image: String
+});
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteAlbums: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Album' }]
+});
+
+let Album = mongoose.model('Album', albumSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Album = Album;
+module.exports.User = User;
